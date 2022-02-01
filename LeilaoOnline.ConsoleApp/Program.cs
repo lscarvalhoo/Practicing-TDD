3e8866cec4c8fae6553a66a2dd82d206b62a5ac8
Leonardo Sousa Carvalho <45200813+lscarvalhoo@users.noreply.github.com>
2022-02-01 11:09:58 -0300

    Alterações nos testes

---
+++ LeilaoOnline.ConsoleApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using LeilaoOnline.Core;
+using System;
+
+namespace LeilaoOnline.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var leilao = new Leilao("Van Gogh");
+            var pessoa = new Interessada("João", leilao);
+            var pessoaDois = new Interessada("Maria", leilao);
+
+            leilao.RecebeLance(pessoa, 800);
+            leilao.RecebeLance(pessoaDois, 900);
+            leilao.RecebeLance(pessoa, 1000);
+            leilao.RecebeLance(pessoaDois, 1100);
+
+            leilao.TerminaPregao();
+
+            Console.WriteLine(leilao.Ganhador.Valor);
+        }
+    }
+}
---
+++ LeilaoOnline.Tests/LeilaoTerminaPregao.cs
@@ -0,0 +1,52 @@
+﻿using LeilaoOnline.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace LeilaoOnline.Tests
+{
+    public class LeilaoTerminaPregao
+    {
+        [Theory]
+        [InlineData(1200, new double[] { 800, 900, 1000, 1200 })]
+        [InlineData(1000, new double[] { 800, 900, 1000, 990 })]
+        [InlineData(800, new double[] { 800 })]
+        public void RetornaMaiorValorLeilaoAoMenosUmLance(double valorEsperado, double[] ofertas)
+        {
+            //Arraje
+            var leilao = new Leilao("Van Gogh");
+            var pessoa = new Interessada("Jao", leilao);
+
+            foreach (var valor in ofertas)
+            {
+                leilao.RecebeLance(pessoa, valor);
+            }
+
+            //Act
+            leilao.TerminaPregao();
+
+            //Assert 
+            var valorObtido = leilao.Ganhador.Valor;
+            Assert.Equal(valorEsperado, valorObtido);
+        }
+
+        [Fact]
+        public void RetornaZeroLeilaoSemLance()
+        {
+            //Arange 
+            var leilao = new Leilao("Van Gogh");
+
+            //Act
+            leilao.TerminaPregao();
+
+            //Assert
+            var valorEsperado = 0;
+            var valorObtido = leilao.Ganhador.Valor;
+
+            Assert.Equal(valorEsperado, valorObtido);
+        }
+    }
+}
